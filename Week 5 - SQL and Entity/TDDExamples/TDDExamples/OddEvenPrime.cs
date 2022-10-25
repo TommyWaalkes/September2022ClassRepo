d5c4010a646594a8e80e75ca6df37c31d8f6bbd5
unknown <tommy@grandcircus.co>
2022-10-25 10:33:18 -0400

    Added TDD example

---
+++ Week 5 - SQL and Entity/TDDExamples/TDDExamples/OddEvenPrime.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDExamples
+{
+    public class OddEvenPrime
+    {
+        public string GetPrintOut(int num)
+        {
+            if(num == 1 || num == 0)
+            {
+                return "SPECIAL CASE";
+            }
+            
+            if( num == 2)
+            {
+                return "PRIME";
+            }
+            //A number is prime if it is Odd and only divisble by 1 and itself
+            if (num % 2 == 0)
+            {
+                return "EVEN";
+            }
+            else
+            {
+                bool IsPrime = true;
+                for(int i = 3; i <num; i += 2)
+                {
+                    if(num % i == 0)
+                    {
+                        IsPrime = false;
+                        break;
+                    }
+                }
+
+                if (IsPrime == false)
+                {
+                    return "ODD";
+                }
+                else
+                {
+                    return "PRIME";
+                }
+            }
+        }
+    }
+}
---
+++ Week 5 - SQL and Entity/TDDExamples/Tests/OddEvenPrimeTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDDExamples;
+using Xunit;
+
+namespace Tests
+{
+    public class OddEvenPrimeTest
+    {
+        [Theory]
+        [InlineData(15, "ODD")]
+        [InlineData(10, "EVEN")]
+        [InlineData(17, "PRIME")]
+        [InlineData(2, "PRIME")]
+        public void TestOddEvenPrime(int num, string expected)
+        {
+            //Arrange
+            OddEvenPrime o = new OddEvenPrime();
+
+            //Act 
+            string actual = o.GetPrintOut(num);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void BatchTest()
+        {
+            //Arrange 
+            OddEvenPrime o = new OddEvenPrime();
+            List<int> primes 
+                = new List<int>() { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+            for(int i = 0; i <=100; i++)
+            {
+                string actual = o.GetPrintOut(i);
+
+                string expected;
+
+                if (i == 1 || i == 0)
+                {
+                    expected = "SPECIAL CASE";
+                }
+                else if (primes.Contains(i))
+                {
+                    expected = "PRIME";
+                }
+                else if(i % 2 == 0)
+                {
+                    expected = "EVEN";
+                }
+                else
+                {
+                    expected = "ODD";
+                }
+
+                Assert.Equal(expected, actual);
+            }
+        }
+    }
+}
