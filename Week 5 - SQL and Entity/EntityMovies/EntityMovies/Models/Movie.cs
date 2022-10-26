fefaafbd5ba4cdb1a4337b35772b6b9f0f57be5f
unknown <tommy@grandcircus.co>
2022-10-26 10:19:56 -0400

    Added Movie DB

---
+++ Week 5 - SQL and Entity/EntityMovies/EntityMovies/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityMovies.Models
+{
+    public partial class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Genre { get; set; }
+        public double? Runtime { get; set; }
+    }
+}
---
+++ Week 5 - SQL and Entity/EntityMovies/EntityMovies/Program.cs
@@ -0,0 +1,43 @@
+﻿using EntityMovies.Models;
+
+namespace EntityMovies
+{
+    internal class Program
+    {
+        //This variable is static so I can use it in static methods 
+        public static Movies2Context db = new Movies2Context();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a Genre you wish to search");
+            string genre = Console.ReadLine();
+            List<Movie> results = SearchByGenre(genre);
+
+            PrintMovies(results);
+
+            Console.WriteLine("Please input a title you wish to search for");
+            string title = Console.ReadLine();
+            List<Movie> results2 = SearchByTitle(title);
+            PrintMovies(results2);
+
+        }
+
+        public static List<Movie> SearchByGenre(string genre)
+        {
+            List<Movie> searchResults = db.Movies.Where(m => m.Genre == genre).ToList();
+            return searchResults;
+        }
+
+        public static List<Movie> SearchByTitle(string title)
+        {
+            return db.Movies.Where(m => m.Title.Contains(title)).ToList();
+        }
+
+        public static void PrintMovies(List<Movie> movies)
+        {
+            for(int i = 0; i < movies.Count; i++)
+            {
+                Console.WriteLine($"{i} : {movies[i].Title}");
+            }
+        }
+    }
+}
