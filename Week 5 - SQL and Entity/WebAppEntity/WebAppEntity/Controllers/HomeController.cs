5ed1c3d327f47c203743498ee4bb7daf4cc5cab8
unknown <tommy@grandcircus.co>
2022-10-28 13:22:36 -0400

    Added MVC w/ Entity example

---
+++ Week 5 - SQL and Entity/WebAppEntity/WebAppEntity/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebAppEntity.Models;
+
+namespace WebAppEntity.Controllers
+{
+    //Outside of errors, .net doesn't really print to the browser console
+    public class HomeController : Controller
+    {
+        Movies2Context db = new Movies2Context();
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Movie m = db.Movies.First();
+            return View(m);
+        }
+
+        public IActionResult MovieList()
+        {
+            List<Movie> movies = db.Movies.ToList();
+            return View(movies);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Week 5 - SQL and Entity/WebAppEntity/WebAppEntity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppEntity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
