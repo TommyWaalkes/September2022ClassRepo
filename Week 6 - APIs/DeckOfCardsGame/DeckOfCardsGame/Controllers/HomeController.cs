6303c2a9e02e33cc99384d195f982e7cfd7501c4
Josh <thejosh2021@gmailcom>
2022-11-02 16:26:28 -0400

    adding card demo

--- Week 6 - APIs/DeckOfCardsGame/DeckOfCardsGame/Controllers/HomeController.cs
+++ Week 6 - APIs/DeckOfCardsGame/DeckOfCardsGame/Controllers/HomeController.cs
@@ -7,6 +7,15 @@ namespace DeckOfCardsGame.Controllers
     public class HomeController : Controller
     {
         DoCDAL api = new DoCDAL();
+
+        enum CARD_VALUE
+        {
+            ACE,
+            KING,
+            QUEEN,
+            JACK
+        }
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -17,6 +26,38 @@ namespace DeckOfCardsGame.Controllers
         public IActionResult Index()
         {
             Draws d = api.GetCards(2);
+            Card card1 = d.cards[0];
+            Card card2 = d.cards[1];
+
+            int numericValue1;
+            int numericValue2;
+
+            // Try to convert to a nubmer, otherwise it is a face card
+            if(!int.TryParse(card1.value, out numericValue1))
+            {
+                CARD_VALUE face = (CARD_VALUE) Enum.Parse(typeof(CARD_VALUE), card1.value);
+                numericValue1 = 14 - (int)face;
+            }
+
+            if (!int.TryParse(card2.value, out numericValue2))
+            {
+                CARD_VALUE face = (CARD_VALUE)Enum.Parse(typeof(CARD_VALUE), card2.value);
+                numericValue2 = 14 - (int)face;
+            }
+
+            if(numericValue1 > numericValue2)
+            {
+                d.Winner = "Player 1 is the winner!";
+            }
+            else if(numericValue1 < numericValue2)
+            {
+                d.Winner = "Player 2 is the winner!";
+            }
+            else
+            {
+                d.Winner = "It is a draw!";
+            }
+
             return View(d);
         }
 
