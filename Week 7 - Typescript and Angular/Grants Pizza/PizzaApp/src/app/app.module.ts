3b8cb5ceca1076410607da4845d527eba0b7cbca
unknown <tommy@grandcircus.co>
2022-11-11 15:23:54 -0500

    Added ngif example

--- Week 7 - Typescript and Angular/Grants Pizza/PizzaApp/src/app/app.module.ts
+++ Week 7 - Typescript and Angular/Grants Pizza/PizzaApp/src/app/app.module.ts
@@ -1,12 +1,16 @@
+//This imports any extension meant to be used site wide. 
+//When we create a new component the command line places a reference to that component in this file
+//We're registering the component so it can be used in our app
+//Tread carefully with this file, breaking this file can break your whole app
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { AddItemComponent } from './add-item/add-item.component';
-import { FormsModule } from '@angular/forms';
 import { DeleteItemComponent } from './delete-item/delete-item.component';
 import { UpdateFormComponent } from './update-form/update-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,6 +20,7 @@ import { UpdateFormComponent } from './update-form/update-form.component';
     DeleteItemComponent,
     UpdateFormComponent
   ],
+  //pull in extension to Angular, most often one built into Angular
   imports: [
     BrowserModule, 
     FormsModule
