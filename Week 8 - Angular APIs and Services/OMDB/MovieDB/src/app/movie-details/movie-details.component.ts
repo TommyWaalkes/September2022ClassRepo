48b8edb6bd73657c3e7b8cc829a9e58d367ded5a
unknown <tommy@grandcircus.co>
2022-11-15 14:01:11 -0500

    Added open movie DB

--- Week 8 - Angular APIs and Services/OMDB/MovieDB/src/app/movie-details/movie-details.component.ts
+++ Week 8 - Angular APIs and Services/OMDB/MovieDB/src/app/movie-details/movie-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MovieDetails } from '../MovieDetails';
 
 @Component({
   selector: 'app-movie-details',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-details.component.css']
 })
 export class MovieDetailsComponent implements OnInit {
-
+  @Input() displayMovie: MovieDetails ={} as MovieDetails;
   constructor() { }
 
   ngOnInit(): void {
--- Week 8 - Angular APIs and Services/OMDB/MovieDB/src/app/search-results/search-results.component.ts
+++ Week 8 - Angular APIs and Services/OMDB/MovieDB/src/app/search-results/search-results.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MovieDBService } from '../movie-db.service';
+import { Search, SearchResult } from '../SearchResult';
+import { MovieDetails } from '../MovieDetails';
 
 @Component({
   selector: 'app-search-results',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchResultsComponent implements OnInit {
 
-  constructor() { }
+  //This allows Search results to get passed down into this component
+  @Input() results: SearchResult[] =[];
+  movieToDisplay: MovieDetails = {} as MovieDetails;
+  constructor(private movieService: MovieDBService) { }
 
   ngOnInit(): void {
   }
+
+  DisplayDetails(s:Search, i:number):MovieDetails{
+   
+    this.movieService.GetMovieDetails(s.Title).subscribe(
+      (m:MovieDetails) => {
+        this.movieToDisplay = m;
+        console.log(m);
+        let display = document.getElementById(i.toString());
+        display?.setAttribute("style", "display:block");
+
+      }
+    );
+
+    return this.movieToDisplay;
+
+  }
+  
 
 }
