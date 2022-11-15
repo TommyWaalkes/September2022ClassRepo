f723b97f862abfe3b288901693000299ebb17f6c
unknown <tommy@grandcircus.co>
2022-11-15 14:23:53 -0500

    Finished movie app

--- Week 8 - Angular APIs and Services/OMDB/MovieDB/src/app/search-results/search-results.component.ts
+++ Week 8 - Angular APIs and Services/OMDB/MovieDB/src/app/search-results/search-results.component.ts
@@ -24,6 +24,15 @@ export class SearchResultsComponent implements OnInit {
       (m:MovieDetails) => {
         this.movieToDisplay = m;
         console.log(m);
+        //this returns an array with every instance display appears 
+        //We want to set all of them to disappear 
+        let detailsPanes = document.getElementsByClassName("display");
+
+        for(let i = 0; i < detailsPanes.length; i++){
+          let p = detailsPanes[i];
+          p.setAttribute("style", "display:none");
+        }
+
         let display = document.getElementById(i.toString());
         display?.setAttribute("style", "display:block");
 
